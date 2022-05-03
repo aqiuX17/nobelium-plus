5148bba0ab744c056de9be013bbc2b33f3eead9f
aqiuX17 <92378955+aqiuX17@users.noreply.github.com>
2022-05-03 15:22:46 +0800

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,208 @@
+import { useEffect, useState, useRef } from 'react'
+import Link from 'next/link'
+import BLOG from '@/blog.config'
+import { lang } from '@/lib/lang'
+import { useRouter } from 'next/router'
+import {
+  HomeIcon,
+  NewspaperIcon,
+  CollectionIcon,
+  SparklesIcon,
+  SearchIcon,
+  MenuIcon
+} from '@heroicons/react/outline'
+import Image from 'next/image'
+import Social from './Social.js'
+import ThemeSwitcher from './ThemeSwitcher.js'
+import LangSwitcher from './LangSwitcher.js'
+
+const NavBar = () => {
+  const router = useRouter()
+  const { locale } = useRouter()
+  const t = lang[locale]
+  const [showMenu, setShowMenu] = useState(false)
+
+  let activeMenu = ''
+  if (router.query.slug) {
+    activeMenu = '/' + router.query.slug
+  } else {
+    activeMenu = router.pathname
+  }
+
+  const links = [
+    {
+      id: 0,
+      name: t.NAV.INDEX,
+      to: BLOG.path || '/',
+      icon: <HomeIcon className='inline-block mb-1 h-5 w-5' />,
+      show: true
+    },
+    {
+      id: 1,
+      name: t.NAV.NEWSLETTER,
+      to: '/newsletter',
+      icon: <NewspaperIcon className='inline-block mb-1 h-5 w-5' />,
+      show: true
+    },
+    {
+      id: 2,
+      name: t.NAV.NOTES,
+      to: '/notes',
+      icon: <CollectionIcon className='inline-block mb-1 h-5 w-5' />,
+      show: true
+    },
+    {
+      id: 3,
+      name: t.NAV.PROJECTS,
+      to: '/projects',
+      icon: <SparklesIcon className='inline-block mb-1 h-5 w-5' />,
+      show: BLOG.showAbout
+    },
+    {
+      id: 4,
+      name: t.NAV.SEARCH,
+      to: '/search',
+      icon: <SearchIcon className='inline-block mb-1 h-5 w-5' />,
+      show: true
+    }
+  ]
+  return (
+    <div className='flex'>
+      <ul className='hidden md:flex md:gap-1'>
+        {links.map(
+          (link) =>
+            link.show && (
+              <Link passHref key={link.id} href={link.to}>
+                <li
+                  className={`${
+                    activeMenu === link.to ? 'bg-gray-200 dark:bg-gray-700' : ''
+                  } hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-lg block py-1 px-2 nav`}
+                >
+                  <a className='font-light'>
+                    {link.icon}
+                    <span className='inline-block m-1'>{link.name}</span>
+                  </a>
+                </li>
+              </Link>
+            )
+        )}
+      </ul>
+
+      <ThemeSwitcher />
+      <LangSwitcher />
+
+      <div className='md:hidden mr-2 block '>
+        <button
+          type='button'
+          onClick={() => setShowMenu((showMenu) => !showMenu)}
+          className='hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-lg block p-2 -mr-3 md:pb-3'
+        >
+          <MenuIcon className='inline-block mb-1 h-5 w-5' />
+        </button>
+        {showMenu && (
+          <div className='absolute right-0 w-40 mr-4 mt-2 origin-top-right bg-white dark:bg-gray-700 divide-y divide-gray-200 dark:divide-gray-600 rounded-md shadow-lg outline-none'>
+            <div className='py-1'>
+              {links.map(
+                (link) =>
+                  link.show && (
+                    <Link passHref key={link.id} href={link.to}>
+                      <a className='hover:bg-gray-100 dark:hover:bg-gray-600 font-light block justify-between w-full px-4 py-2 leading-5'>
+                        {link.icon}
+                        <span className='m-1'>{link.name}</span>
+                      </a>
+                    </Link>
+                  )
+              )}
+            </div>
+            <div className='px-4 py-4'>
+              <Social />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+const Header = ({ navBarTitle, fullWidth }) => {
+  const [showTitle, setShowTitle] = useState(false)
+  const useSticky = !BLOG.autoCollapsedNavBar
+  const navRef = useRef(null)
+  const sentinalRef = useRef([])
+  const handler = ([entry]) => {
+    if (navRef && navRef.current && useSticky) {
+      if (!entry.isIntersecting && entry !== undefined) {
+        navRef.current?.classList.add('sticky-nav-full')
+      } else {
+        navRef.current?.classList.remove('sticky-nav-full')
+      }
+    } else {
+      navRef.current?.classList.add('remove-sticky')
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > 100) {
+        setShowTitle(true)
+      } else {
+        setShowTitle(false)
+      }
+    })
+
+    const obvserver = new window.IntersectionObserver(handler)
+    obvserver.observe(sentinalRef.current)
+    // Don't touch this, I have no idea how it works XD
+    // return () => {
+    //   if (sentinalRef.current) obvserver.unobserve(sentinalRef.current)
+    // }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sentinalRef])
+  return (
+    <>
+      <div className='observer-element h-4 md:h-12' ref={sentinalRef}></div>
+      <div
+        className={`sticky-nav m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60 ${
+          !fullWidth ? 'max-w-3xl px-4' : 'px-4 md:px-24'
+        }`}
+        id='sticky-nav'
+        ref={navRef}
+      >
+        <div className='flex items-center'>
+          <Link passHref href='/'>
+            <a aria-label={BLOG.title}>
+              <div className='h-6 hover:text-blue-500 dark:hover:text-blue-500 fill-current'>
+                <Image
+                  src='/apple-touch-icon.png'
+                  alt='Logo'
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </a>
+          </Link>
+          {navBarTitle ? (
+            <p
+              className={`ml-2 font-medium ${
+                !showTitle ? 'hidden' : 'hidden xl:block'
+              }`}
+            >
+              {navBarTitle}
+            </p>
+          ) : (
+            <p
+              className={`ml-2 font-medium ${
+                !showTitle ? 'hidden' : 'hidden xl:block'
+              }`}
+            >
+              {BLOG.title},{' '}
+              <span className='font-normal'>{BLOG.description}</span>
+            </p>
+          )}
+        </div>
+        <NavBar />
+      </div>
+    </>
+  )
+}
+
+export default Header
